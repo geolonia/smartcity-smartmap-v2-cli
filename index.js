ec569f6312eacd64b52444928d4a83c53f6f1bd9
naoki <n.globe.us@gmail.com>
2024-08-02 12:23:08 +0900

    変数名を変更

--- index.js
+++ index.js
@@ -4,16 +4,16 @@ const { config } = require('process');
 
 // データ階層ファイルのパス
 const configFilePath = process.argv[2];
-const smartMapInputPath = process.argv[3];
-const smartMapOutputPath = process.argv[4];
+const InputPath = process.argv[3];
+const OutputPath = process.argv[4];
 
 const main = async () => {
 
     const smartMapUtil = new SmartMapUtil(
         {
             configFilePath: path.join(__dirname, configFilePath || 'smartcity-data.xlsx'),
-            dataDir: path.join(__dirname, smartMapInputPath || 'data'),
-            mbtilesPath: path.join(__dirname, smartMapOutputPath || 'smartcity-poc.mbtiles'),
+            dataDir: path.join(__dirname, InputPath || 'data'),
+            mbtilesPath: path.join(__dirname, OutputPath || 'smartcity-poc.mbtiles'),
         }
     );
 
